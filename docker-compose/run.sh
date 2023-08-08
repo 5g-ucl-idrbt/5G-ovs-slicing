71ab06e742e1a3ac718ea6f3709e36e06f0bceee
V Abhishek Raj <46273637+abhic137@users.noreply.github.com>
2023-08-08 20:31:07 +0530

    Update run.sh

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "========================================================================================================="
 cat << "EOF"
 
 
@@ -17,16 +17,19 @@ cat << "EOF"
        \/        \/       \/             \/        \//_____/                                   
 
 EOF
-
+echo "========================================================================================================="
 # creating links with the createlink.sh file
 sudo bash createLink.sh oai-spgwu s1
 sudo bash createLink.sh server s1
 sudo bash createLink.sh router s1
+echo "========================================================================================================="
 echo "***************LINKS ARE CREATED**************"
-
+echo "========================================================================================================="
 # del bridge in s1 if it already exits
 sudo docker exec s1 ovs-vsctl del-br br0
+echo "========================================================================================================="
 echo "********************CHECKING/DELETING EXISTING BRIDGE********************"
+echo "========================================================================================================="
 # creating bridge adding ports to bridge
 BRIDGE_NAME="br0"
 PORTS=$(sudo ip netns exec s1 ifconfig -a | grep -E "dcp[0-9]+" | awk -F':' '{print $1}')
@@ -38,9 +41,9 @@ for PORT in $PORTS; do
 done
 
 sudo docker exec s1 ovs-vsctl set-fail-mode $BRIDGE_NAME secure
-
+echo "========================================================================================================="
 echo "*********************BRIDGE IS CREATED AND PORTS ARE ADDED************************"
-
+echo "========================================================================================================="
 
 
 #INTF1=$(sudo ip netns exec s1 ifconfig -a | grep -E "dcp.*" | awk -F':' '{print $1}')
@@ -58,18 +61,23 @@ C2_IF=$(sudo ip netns exec server ifconfig -a | grep -E "dcp.*" | awk -F':' '{pr
 sudo ip netns exec server ip a add 10.0.0.2/24 dev ${C2_IF}
 C3_IF=$(sudo ip netns exec router ifconfig -a | grep -E "dcp.*" | awk -F':' '{print $1}')
 sudo ip netns exec router ip a add 10.0.0.3/24 dev ${C3_IF}
+echo "========================================================================================================="
 echo "***************************IPS ARE ADDED TO SPGWU,SERVER & ROUTER **************************"
+echo "========================================================================================================="
 
 sudo ip netns exec oai-spgwu ip r add 10.0.0.3/32 via 0.0.0.0 dev ${C1_IF}
 sudo ip netns exec oai-spgwu ip r add 10.0.0.2/32 via 0.0.0.0 dev ${C1_IF}
 sudo ip netns exec server ip r add 10.0.0.1/32 via 0.0.0.0 dev ${C2_IF}
 sudo ip netns exec router ip r add 10.0.0.1/32 via 0.0.0.0 dev ${C3_IF}
+echo "========================================================================================================="
 echo "******************************ROUTES ARE ADDED TO SPGWU,SERVER & ROUTER****************************"
+echo "========================================================================================================="
+
 # Add controller to the ovs
 sudo docker exec s1 ovs-vsctl set-controller br0 tcp:172.18.0.4:6653
-
+echo "========================================================================================================="
 echo "***********************RYU CONTROLLER IS SET ************************"
-
+echo "========================================================================================================="
 #spgwu configs
 sudo docker exec oai-spgwu apt update
 sudo docker exec oai-spgwu apt install -y iputils-ping
@@ -81,7 +89,9 @@ sudo docker exec oai-spgwu iptables -P FORWARD ACCEPT
 sudo docker exec oai-spgwu ip route del default via 192.168.70.129 dev eth0
 sudo ip netns exec oai-spgwu ip route add default via 10.0.0.3 dev ${C1_IF}
 #sudo docker exec <container_name_or_id> sh -c 'command1 && command2 && command3'
+echo "========================================================================================================="
 echo "*************************SPGWU CONFIGURATION IS DONE*******************************"
+echo "========================================================================================================="
 
 #server configs
 sudo docker exec server apt update
@@ -94,8 +104,9 @@ sudo docker exec server apt-get install -y python3
 sudo docker exec server apt install -y wget
 sudo docker exec server ip route del default via 192.168.70.129 dev eth0
 sudo ip netns exec server ip r add 12.1.1.0/24 via 10.0.0.1 dev ${C2_IF}
+echo "========================================================================================================="
 echo "**************************SERVER CONFIGURATION IS DONE************************"
-
+echo "========================================================================================================="
 #router configs
 sudo docker exec router apt update
 sudo docker exec router apt install -y iputils-ping
@@ -104,8 +115,10 @@ sudo docker exec router apt install -y iproute2
 sudo docker exec router apt install -y iptables 
 sudo docker exec router apt install -y net-tools
 sudo ip netns exec router ip r add 12.1.1.0/24 via 10.0.0.1 dev ${C3_IF}
+echo "========================================================================================================="
 echo "************************ROUTER CONFIGURATION IS DONE***********************"
-
+echo "========================================================================================================="
+echo "========================================================================================================="
 cat << "EOF"
 
 
@@ -118,4 +131,5 @@ cat << "EOF"
                                                                                    
                                                                                    
 EOF
+echo "========================================================================================================="
 #github:abhic137
